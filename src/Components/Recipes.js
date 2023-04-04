d8fe7981631c187c509b44e60e0d375cc2d92568
Calypsea <austeja.mintak@gmail.com>
2023-04-04 17:19:52 +0300

    fixed title clipping

--- src/Components/Recipes.js
+++ src/Components/Recipes.js
@@ -10,7 +10,7 @@ export default function Recipes(props){
     }
     
     return(
-        <div className="recipe">
+        <div className="recipe" >
             <img src={props.url} alt={props.name} className="recipe_img"/>
             <h2 className="recipe_title">{props.name}</h2>
             
